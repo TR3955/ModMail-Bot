e433ca9d4e14b8279eba53e241bac3ebbc16ab23
TR3955 <trtr05863@gmail.com>
2025-05-08 20:20:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     client: {
         token: '', // ← Your bot token (.env IS RECOMMENDED)
-        id: '1354494077437219008' // ← Your bot ID
+        id: '1370050201523458138' // ← Your bot ID
     },
     modmail: {
-        guildId: '1315655498581676052', // ← Your server ID
-        categoryId: '1356682208735133756', // ← The modmail category ID
-        staffRoles: ['1329023423736844329'], // ← The modmail staff roles IDs
+        guildId: '1293569663694930011', // ← Your server ID
+        categoryId: '1354437299261145139', // ← The modmail category ID
+        staffRoles: ['1313760129119813633'], // ← The modmail staff roles IDs
         mentionStaffRolesOnNewMail: true // ← Mention staff roles when there is a new mail?
     },
     logs: {
